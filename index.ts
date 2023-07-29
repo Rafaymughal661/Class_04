fd8c292630c9a5b11ca53f4468d1fbbadc206330
RafayMughal <86525540+Rafaymughal661@users.noreply.github.com>
2023-07-30 01:05:13 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,71 @@
+// FUNCTION
+// function cals():string
+// {
+//     return "ABDUL RAFAY"
+// }
+// console.warn(cals());
+
+
+// ARRAY:
+// let ALL:string[] = ["Ali", "Ahmad", "Arslan", "ayzel"];
+// console.warn(ALL);
+ 
+
+// OBJECT:
+// interface usersTyped{
+//    name:string,
+//    class:number,
+//    Section:string,
+// }
+
+// let users:usersTyped={
+//     name: "RAFAY",
+//     class: 12,
+//     Section: "BI-II",
+// }
+// users.name = "Ali";
+// console.warn(users);
+
+
+// UNION TYPES
+// var school:(string|number|boolean) = "THE EDUCATOR";
+// school = 100;
+// school = true;
+// console.warn(school);
+
+
+// FUNCTION:
+// function tools(a:number,b?:number) {
+//     return b?a+b:a;
+// };
+// console.log(111);
+
+// function isDivisibleBy4And8(num: number): boolean {
+//     return num % 4 === 0 && num % 8 === 0;
+//   }
+  
+//   // Test cases
+//   console.log(isDivisibleBy4And8(16)); // Output: true (16 is divisible by both 4 and 8)
+//   console.log(isDivisibleBy4And8(32)); // Output: false (32 is divisible by 4 but not by 8)
+//   console.log(isDivisibleBy4And8(64)); // Output: true (64 is divisible by both 4 and 8)
+//   console.log(isDivisibleBy4And8(10)); 
+
+// BIGINT:
+// let maxnum:bigint = 100212355585545556622n;
+// console.warn(maxnum);
+
+var anotherbignum = BigInt("1000300050603030020210");
+
+console.log(anotherbignum);
+
+
+
+
+
+
+
+
+
+
+
+
